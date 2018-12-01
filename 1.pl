55a501f2f9b17c534e2e683c6714ba6712f9bd52
broomweed <milesabaker@gmail.com>
2018-12-01 15:59:23 -0800

    day 1.

---
+++ 1.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use List::Util qw( sum );
+use v5.010;
+
+my @nums = grep {$_ ne "\n"} <>;
+
+say "Part 1: ", sum @nums;
+
+my %used;
+my $total = 0;
+rep: while (1) {
+    for (@nums) {
+        $total += $_;
+        last rep if $used{$total};
+        $used{$total} = 1;
+    }
+}
+
+say "Part 2: $total";
