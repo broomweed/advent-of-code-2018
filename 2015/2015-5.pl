ca82d13b3c0e5f5b6d7ba453da9c09b4aec29e10
broomweed <milesabaker@gmail.com>
2018-12-01 19:29:24 -0800

    nicer 2015-5

--- 2015/2015-5.pl
+++ 2015/2015-5.pl
@@ -3,28 +3,20 @@ use v5.010;
 
 @strings = <>;
 
-for (@strings) {
-    $nice ++ if nice($_);
-}
+say scalar grep { nice1($_) } @strings;
+say scalar grep { nice2($_) } @strings;
 
-say $nice;
-
-for (@strings) {
-    $nice2 ++ if nice2($_);
-}
-
-say $nice2;
-
-sub nice {
-    # whoa, dynamic scoping! :/
-    return 0 if $_ !~ /(\w)\1/;
-    return 0 if /ab|cd|pq|xy/;
-    return 0 if $_ !~ /[aeiou].*[aeiou].*[aeiou]/;
+# part 1
+sub nice1 {
+    return 0 if $_[0] !~ /(\w)\1/;
+    return 0 if $_[0] =~ /ab|cd|pq|xy/;
+    return 0 if $_[0] !~ /[aeiou].*[aeiou].*[aeiou]/;
     return 1;
 }
 
+# part 2
 sub nice2 {
-    return 0 if $_ !~ /(\w\w).*\1/;
-    return 0 if $_ !~ /(\w).\1/;
+    return 0 if $_[0] !~ /(\w\w).*\1/;
+    return 0 if $_[0] !~ /(\w).\1/;
     return 1;
 }
